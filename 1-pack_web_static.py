8dbe471de35d91ba8a5cbc80d2dbaca3c6283e81
MSI <wyeabkal@gmail.com>
2023-07-09 15:41:15 +0300

    gai

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""python script that uses fabric to generate .tgz"""
-
 from fabric.api import local
-from datetime import datetime
-from fabric.decorators import runs_once
+from time import strftime
+from datetime import date
 
 
-@runs_once
 def do_pack():
-    '''uses fabric to generate .tgz for web_static folder'''
-    local("mkdir -p versions")
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
-    location = ("versions/web_static_{}.tgz".format(time))
-    resp = local("tar -cvzf {} web_static".format(location))
-    if resp.failed:
+    """ A script that generates archive the contents of web_static folder"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
         return None
-    return location
